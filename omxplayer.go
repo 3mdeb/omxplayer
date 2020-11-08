1791e0bda7a056e960a91c2b37beb25b458d0660
Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>
2020-11-08 20:47:09 +0100

    omxplayer.go: fix syntax
    Signed-off-by: Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>

--- omxplayer.go
+++ omxplayer.go
@@ -51,7 +51,7 @@ func New(url string, args ...string) (player *Player, err error) {
 	// omxplayer can and should use existing dbus files, removing them cause
 	// omxplayer script to create new ones which causes new dbus-daemon process
 	// to create without properly closing the previous one
-	//removeDbusFiles()
+	// removeDbusFiles()
 
 	cmd, err := execOmxplayer(url, args...)
 	if err != nil {
